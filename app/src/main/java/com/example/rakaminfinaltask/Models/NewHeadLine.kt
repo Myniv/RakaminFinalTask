56f4c1bbcf5b90b85f2ec468a4fe83090bf41831
mulyanan26 <mulyanan.ur26@gmail.com>
2023-12-17 12:08:39 +0700

    Feat: Add API Data Class

---
+++ app/src/main/java/com/example/rakaminfinaltask/Models/NewHeadLine.kt
@@ -0,0 +1,12 @@
+package com.example.rakaminfinaltask.Models
+
+data class NewHeadLine(
+    var source: Source?,
+    var author: String?,
+    var title: String?,
+    var description: String?,
+    var url: String?,
+    var urlToImage: String?,
+    var publishedAt: String?,
+    var content: String?,
+)
