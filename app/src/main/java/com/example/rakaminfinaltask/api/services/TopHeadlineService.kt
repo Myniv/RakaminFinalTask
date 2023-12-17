e39948b774768923a18f13e8ab6ac381371ef271
mulyanan26 <mulyanan.ur26@gmail.com>
2023-12-17 22:10:45 +0700

    Feat: Add TopHeadlineService

---
+++ app/src/main/java/com/example/rakaminfinaltask/api/services/TopHeadlineService.kt
@@ -0,0 +1,10 @@
+package com.example.rakaminfinaltask.api.services
+
+import com.example.rakaminfinaltask.api.models.NewsApiResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface TopHeadlineService {
+    @GET("v2/top-headlines?country=us&apiKey=b386fab884a846d3893d51ecfe35e05f")
+    fun getAll(): Call<NewsApiResponse>
+}
