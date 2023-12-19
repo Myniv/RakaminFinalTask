a9bb3d4a2cd2e99f092256b062ff5e2c14d6d130
mulyanan26 <mulyanan.ur26@gmail.com>
2023-12-19 23:55:54 +0700

    Feat : Add NavBar

--- app/src/main/java/com/example/rakaminfinaltask/NavBar.kt
+++ app/src/main/java/com/example/rakaminfinaltask/NavBar.kt
@@ -2,10 +2,35 @@ package com.example.rakaminfinaltask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.rakaminfinaltask.databinding.ActivityNavBarBinding
 
 class NavBar : AppCompatActivity() {
+    private lateinit var binding: ActivityNavBarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nav_bar)
+        binding = ActivityNavBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(MainActivity())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(MainActivity())
+                R.id.about -> replaceFragment(AboutPageMe())
+
+                else ->{
+
+
+                }
+            }
+                true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
